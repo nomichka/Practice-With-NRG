708e39c0f737cab4fa16b81c9dc54f081937a098
ReginaLin2005 <74690711+ReginaLin2005@users.noreply.github.com>
2020-11-18 19:13:46 -0800

    Blocked

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("You have been blocked by user \"World\":(");
     }
 }
